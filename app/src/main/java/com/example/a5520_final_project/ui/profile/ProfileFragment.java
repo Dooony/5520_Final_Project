8eaa9f4fa43501cddcd1f65a56676e37af42bafc
Dooony <feng.xuey@northeastern.edu>
2024-04-21 00:56:39 -0400

    Profile Frag

--- app/src/main/java/com/example/a5520_final_project/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/a5520_final_project/ui/profile/ProfileFragment.java
@@ -1,6 +1,7 @@
 package com.example.a5520_final_project.ui.profile;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,21 +12,48 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.a5520_final_project.databinding.FragmentProfileBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileFragment extends Fragment {
 
     private FragmentProfileBinding binding;
+    private FirebaseAuth mAuth;
+    private DatabaseReference markersRef;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        ProfileViewModel profileViewModel =
-                new ViewModelProvider(this).get(ProfileViewModel.class);
-
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            String userEmail = currentUser.getEmail();
+            binding.textEmail.setText("Email/Username: " + userEmail);
+
+            String userId = currentUser.getUid();
+            markersRef = FirebaseDatabase.getInstance().getReference("users").child(userId).child("markers");
+            markersRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    long numMarkers = dataSnapshot.getChildrenCount();
+                    binding.textMarkers.setText("Totoal Markers: " + String.valueOf(numMarkers));
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    // Handle database error
+                    Log.e("ProfileFragment", "Error loading markers from Firebase: " + databaseError.getMessage());
+                }
+            });
+        }
+
         return root;
     }
 
